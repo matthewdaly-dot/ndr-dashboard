fcb239b0796daa369bc104390c57515218d219df
matthewdaly-dot <matthew.daly@samba.tv>
2026-04-01 08:59:58 -0400

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import NDRChart from "./NDRChart.jsx"
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode><NDRChart /></React.StrictMode>
+)
