83ad41551edb1793e3258334ec58faa211f75c78
matthewdaly-dot <matthew.daly@samba.tv>
2026-04-01 09:00:30 -0400

    feat: add React entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import NDRChart from "./NDRChart.jsx"
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><NDRChart /></React.StrictMode>
-)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import NDRChart from './NDRChart.jsx'
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><NDRChart /></React.StrictMode>)
